8896c3cdaf90a39579995f7c733166e1dbddd51c
danicaro94 <danicaro94@gmail.com>
2025-06-18 12:40:11 +0200

    Change image caption

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.write("Welcome to your expedition to the Himalayas! Tell us a bit about yourself and we'll recommend the perfect mountain for you to climb according to your profile.")
-st.image('climbing everest.jpg', caption="Online image", use_container_width=True)
+st.image('climbing everest.jpg', caption="This could be you", use_container_width=True)
 age = st.text_input("How old are you?")
 try:
     age = int(age)
