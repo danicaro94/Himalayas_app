f72fe3f0be6eba87383fd618d4a9342f739b00b6
danicaro94 <danicaro94@gmail.com>
2025-06-19 12:16:13 +0200

    change to mapbox

--- app.py
+++ app.py
@@ -316,9 +316,9 @@ if st.button("🚀 Confirm and Continue"):
     ### Add map
 
     df_map=pd.read_csv('peak_coord_1.csv')
-    df_map.columns
     df_map=df.sample(n=3)
-    fig=px.scatter_map(df_map,lat='latitude', lon= 'longitude',size='success_rate',color='success_rate',hover_data='pkname')
+    fig=px.scatter_mapbox(df_map,lat='latitude', lon= 'longitude',size='success_rate',color='success_rate',hover_data='pkname')
+    fig.update_layout(mapbox_style="open-street-map")
     st.plotly_chart(fig)
     
     #st.write(country_max_height)
